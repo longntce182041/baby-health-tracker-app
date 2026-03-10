74b73dc4f055b792bcbac7899a8b164d19b52ee1
longntce182041 <longntce182041@fpt.edu.vn>
2026-03-10 22:07:01 +0700

    Refactor consultation model and update date input styles in DoctorDashboard

--- baby-health-tracker-be/models/consultations.js
+++ baby-health-tracker-be/models/consultations.js
@@ -1,15 +1,26 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const conversationsSchema = new Schema({
-    parent_id: { type: Schema.Types.ObjectId, ref: 'Parents', required: true },
-    baby_id: { type: Schema.Types.ObjectId, ref: 'Babies', required: true },
-    doctor_id: { type: Schema.Types.ObjectId, ref: 'Doctors', required: true },
-    schedule_id: { type: Schema.Types.ObjectId, ref: 'DoctorSchedules', required: true },
-    status: { type: String, enum: ['waiting', 'in_progress', 'completed', 'canceled'], default: 'waiting' },
+const conversationsSchema = new Schema(
+  {
+    parent_id: { type: Schema.Types.ObjectId, ref: "Parents", required: true },
+    baby_id: { type: Schema.Types.ObjectId, ref: "Babies", required: true },
+    doctor_id: { type: Schema.Types.ObjectId, ref: "Doctors", required: true },
+    schedule_id: {
+      type: Schema.Types.ObjectId,
+      ref: "DoctorSchedules",
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["waiting", "in_progress", "completed", "canceled"],
+      default: "waiting",
+    },
     consultation_time: { type: Date, required: true },
     notes: { type: String },
-}, { timestamps: true });
+  },
+  { timestamps: true },
+);
 
-const Consultations = mongoose.model('Consultations', conversationsSchema);
+const Consultations = mongoose.model("Consultations", conversationsSchema);
 module.exports = Consultations;
